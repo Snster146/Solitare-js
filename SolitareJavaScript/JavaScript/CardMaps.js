bf304e481e294ac54acd8e27960ffe9a13d0cbde
Snster146 <sonnynormansell@gmail.com>
2026-03-09 02:10:53 +0000

    Add files via upload

---
+++ SolitareJavaScript/JavaScript/CardMaps.js
@@ -0,0 +1,68 @@
+var HeartCards= new Map([
+    ["H2","../images/HeartsCardImages/hearts_2.png"],
+    ["H3","../images/HeartsCardImages/hearts_3.png"],
+    ["H4","../images/HeartsCardImages/hearts_4.png"],
+    ["H5","../images/HeartsCardImages/hearts_5.png"],
+    ["H6","../images/HeartsCardImages/hearts_6.png"],
+    ["H7","../images/HeartsCardImages/hearts_7.png"],
+    ["H8","../images/HeartsCardImages/hearts_8.png"],
+    ["H9","../images/HeartsCardImages/hearts_9.png"],
+    ["H10","../images/HeartsCardImages/hearts_10.png"],
+    ["HA","../images/HeartsCardImages/hearts_ace.png"],
+    ["HJ","../images/HeartsCardImages/hearts_jack.png"],
+    ["HK","../images/HeartsCardImages/hearts_king.png"],
+    ["HQ","../images/HeartsCardImages/hearts_queen.png"],
+]);
+
+var SpadeCards= new Map([
+    ["S2","../images/SpadesCardImages/spades_2.png"],
+    ["S3","../images/SpadesCardImages/spades_3.png"],
+    ["S4","../images/SpadesCardImages/spades_4.png"],
+    ["S5","../images/SpadesCardImages/spades_5.png"],
+    ["S6","../images/SpadesCardImages/spades_6.png"],
+    ["S7","../images/SpadesCardImages/spades_7.png"],
+    ["S8","../images/SpadesCardImages/spades_8.png"],
+    ["S9","../images/SpadesCardImages/spades_9.png"],
+    ["S10","../images/SpadesCardImages/spades_10.png"],
+    ["SA","../images/SpadesCardImages/spades_ace.png"],
+    ["SJ","../images/SpadesCardImages/spades_jack.png"],
+    ["SK","../images/SpadesCardImages/spades_king.png"],
+    ["SQ","../images/SpadesCardImages/spades_queen.png"],
+]);
+
+var ClubCards= new Map([
+    ["C2","../images/ClubCardImages/clubs_2.png"],
+    ["C3","../images/ClubCardImages/clubs_3.png"],
+    ["C4","../images/ClubCardImages/clubs_4.png"],
+    ["C5","../images/ClubCardImages/clubs_5.png"],
+    ["C6","../images/ClubCardImages/clubs_6.png"],
+    ["C7","../images/ClubCardImages/clubs_7.png"],
+    ["C8","../images/ClubCardImages/clubs_8.png"],
+    ["C9","../images/ClubCardImages/clubs_9.png"],
+    ["C10","../images/ClubCardImages/clubs_10.png"],
+    ["CA","../images/ClubCardImages/clubs_ace.png"],
+    ["CJ","../images/ClubCardImages/clubs_jack.png"],
+    ["CK","../images/ClubCardImages/clubs_king.png"],
+    ["CQ","../images/ClubCardImages/clubs_queen.png"],
+]);
+
+var DiamondCards= new Map([
+    ["D2","../images/DiamondCardImages/diamonds_2.png"],
+    ["D3","../images/DiamondCardImages/diamonds_3.png"],
+    ["D4","../images/DiamondCardImages/diamonds_4.png"],
+    ["D5","../images/DiamondCardImages/diamonds_5.png"],
+    ["D6","../images/DiamondCardImages/diamonds_6.png"],
+    ["D7","../images/DiamondCardImages/diamonds_7.png"],
+    ["D8","../images/DiamondCardImages/diamonds_8.png"],
+    ["D9","../images/DiamondCardImages/diamonds_9.png"],
+    ["D10","../images/DiamondCardImages/diamonds_10.png"],
+    ["DA","../images/DiamondCardImages/diamonds_ace.png"],
+    ["DJ","../images/DiamondCardImages/diamonds_jack.png"],
+    ["DK","../images/DiamondCardImages/diamonds_king.png"],
+    ["DQ","../images/DiamondCardImages/diamonds_queen.png"],
+]);
+
+
+
+var Allcard=[HeartCards,SpadeCards,ClubCards,DiamondCards]
+export { HeartCards, SpadeCards, ClubCards, DiamondCards ,Allcard};
